d520034ab2dfbf4bfb6f0cf3728a4a3150468314
nurinemilia <nurinemilia@gmail.com>
2022-02-22 18:17:30 +0800

    update by group nurin

--- TASK7.py
+++ TASK7.py
@@ -3,7 +3,7 @@ import scrapy
 
 class NewSpider(scrapy.Spider):
     name = "new_spider"
-    start_urls = ['https://brickset.com/sets/year-2017']
+    start_urls = ['https://brickset.com/sets/year-2018']
 
     def parse(self, response):
         css_selector = 'img'
