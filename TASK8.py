6e27af2556cbaa16898715fbe738b868f90b8d2e
shahafiz123 <shahafiz2001@gmail.com>
2022-03-01 03:46:27 +0800

    script done by A'LAMIN (27). Unable to commit and push on own github

---
+++ TASK8.py
@@ -0,0 +1,10 @@
+#This is to import the unittest program
+import unittest
+#This is to test the following application specificially to a particular set of inputs
+class scrapyTest(unittest.TestCase):
+
+    def test_EngineType(self):
+        print("Testing")
+# To execute commands from this module
+if __name__ == '__main__':
+    unittest.main()
